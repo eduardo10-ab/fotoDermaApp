0d0555d3817895e0cc57413d6c4299d4501dead4
Eduardo <eduardofuentes2610@gmail.com>
2025-09-01 00:59:27 -0600

    fix para deploy netlify

--- frontend/src/services/firebase.js
+++ frontend/src/services/firebase.js
@@ -9,7 +9,8 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiUrl: process.env.REACT_APP_API_URL
 };
 
 // Inicializar Firebase
